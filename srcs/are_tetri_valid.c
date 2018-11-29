3032468618c844998aad8c3d86918c224d04c2a1
Antoine BARTHELEMY <abarthel@e2r4p5.42.fr>
2018-11-29 16:31:54 +0100

    ok

--- srcs/are_tetri_valid.c
+++ srcs/are_tetri_valid.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/29 16:09:24 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/29 16:31:31 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,43 +47,4 @@ static int		tetriminos(int fd)
 //	test joins
 //
 	return (1);
-}
-
-int		wrap_test(char *file_path)
-{
-	int	fd;
-	int	check;
-
-	if ((fd = open(file_path, O_RDONLY)) < 0)
-		return (-1);
-	check = format_test(fd);
-	if ((close(fd)) < 0)
-		return (-1);
-	if (check == -1)
-		return (-1);
-	if ((fd = open(file_path, O_RDONLY)) < 0)
-		return (-1);
-	check = char_to_tetri(fd);
-	if ((close(fd)) < 0)
-		return (-1);
-	else
-		return (1);
-}
-
-
-/*
-** Main for testings
-*/
-
-int		main(int argc, char **argv)
-{
-	int		ret;
-
-	ret = 0;
-	if (argc == 2)
-	{
-		ret = wrap_test(argv[1]);
-		printf("format_test return: %d\n", ret);
-	}
-	return (0);
 }
