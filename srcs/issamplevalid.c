4dce0e21c06ddf88037e136f048f39cfdeffa4a5
Antoine BARTHELEMY <abarthel@e2r4p5.42.fr>
2018-11-29 14:02:49 +0100

    push

--- srcs/issamplevalid.c
+++ srcs/issamplevalid.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/29 12:57:34 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/29 14:01:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,47 @@
 //count nb of pieces
 
 //#include "fillit.h"
-#define BUFF_SIZE 1
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
+#define BUFF_SIZE 1 // temporary
+#include <fcntl.h> // temp
+#include <unistd.h> // temp
+#include <stdio.h> // temp
 
-int		format_test(char *file_path)
+int		format_test(int fd)
 {
-	int		fd;
 	int		ret;
 	int		i;
 	char	buffer[BUFF_SIZE];
 
+	i = 0;
+	while ((ret = (int)read(fd, buffer, BUFF_SIZE)) > 0 && i < 21)
+	{
+		if ((i == 20 || (i + 1) % 5 == 0) && *buffer != '\n')
+			return (-1);
+		else if (i != 20 && ((i + 1) % 5 != 0 && !(*buffer == '#'
+						|| *buffer == '.')))
+			return (-1);
+		write(1, &buffer[0], 1); // DEBUGG
+		i = (i + 1) % 21; 
+	}
+	return (1);
+}
+
+int		wrap_test(char *file_path)
+{
+	int	fd;
+	int	check;
+
 	if ((fd = open(file_path, O_RDONLY)) < 0)
 		return (-1);
+	check = format_test(fd);
+	if ((close(fd)) < 0)
+		return (-1);
+	if (check == -1)
+		return (-1);
 	else
-	{
-		i = 0;
-		while ((ret = (int)read(fd, buffer, BUFF_SIZE)) > 0 && i < 21)
-		{
-			if ((i == 20 || (i + 1) % 5 == 0) && *buffer != '\n')
-				return (-1);
-			else if (i != 20 && ((i + 1) % 5 != 0 && !(*buffer == '#'
-							|| *buffer == '.')))
-				return (-1);
-			write(1, &buffer[0], 1); // DEBUGG
-			i = (i + 1) % 21; 
-		}
 		return (1);
-	}
 }
+
 
 /*
 ** Main for testings
@@ -56,7 +67,7 @@ int		main(int argc, char **argv)
 	ret = 0;
 	if (argc == 2)
 	{
-		ret = format_test(argv[1]);
+		ret = wrap_test(argv[1]);
 		printf("format_test return: %d\n", ret);
 	}
 	return (0);
