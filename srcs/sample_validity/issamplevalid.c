55481593bcbd5e87009689d589b9f98f90410dd2
Ant0wan <abarthel@student.42.fr>
2018-11-30 17:41:27 +0100

    norme ok and counts

--- srcs/sample_validity/issamplevalid.c
+++ srcs/sample_validity/issamplevalid.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 14:29:58 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/30 16:11:21 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/30 17:41:04 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 int			issamplevalid(char *argv)
 {
 	int		fd;
-	int		ret;
+	int		nb_tetriminos;
 	char	buf[BLOCK_SIZE];
 
+	nb_tetriminos = 0;
 	if ((fd = open(argv, O_RDONLY)) == -1)
 		return (-1);
 	else if ((ft_format_test(fd)) == -1)
@@ -27,12 +28,15 @@ int			issamplevalid(char *argv)
 	else if ((fd = open(argv, O_RDONLY)) == -1)
 		return (-1);
 	while ((read(fd, buf, BLOCK_SIZE)) > 0)
-		if ((ret = ft_is_tetri_valid(ft_block_to_tetri(buf))) == -1)
+	{
+		if ((ft_is_tetri_valid(ft_block_to_tetri(buf))) == -1)
 		{
 			close(fd);
 			return (-1);
 		}
+		++nb_tetriminos;
+	}
 	if ((close(fd)) == -1)
 		return (-1);
-	return (ret);
+	return (nb_tetriminos);
 }
