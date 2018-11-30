5dd5ed5e2d59aae5af79678f4afdf6c65e96d8ca
Antoine BARTHELEMY <abarthel@e2r2p5.42.fr>
2018-11-30 14:17:14 +0100

    test links ok

--- srcs/ft_tetri_validity.c
+++ srcs/ft_tetri_validity.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/30 13:45:43 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/30 14:16:49 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <unistd.h> // temp
 #include <stdio.h> // temp
 
-enum	e_masks
+enum	e_masks // TO DEL 'CAUSE IN .H ALREADY
 {
 		h_mask = 0b0000000000000011,
 		v_mask = 0b0000000000000001,
@@ -28,33 +28,44 @@ enum	e_masks
 static int		ft_count_hlinks(unsigned short tetriminos)
 {
 	int		links_nb;
-	int		move;
+	int		i;
 
 	links_nb = 0;
-	move = 0;
-	while (move < 15)
+	i = 0;
+	while (i < 15)
 	{
-		links_nb = (tetriminos >> move & h_mask) == 3 ? links_nb + 1 : links_nb;
-		++move;
+		links_nb = (tetriminos >> i & h_mask) == 3 ? links_nb + 1 : links_nb;
+		++i;
 	}
 	return (links_nb);
 }
 
 static int		ft_count_vlinks(unsigned short tetriminos)
 {
-	int		links_nb;
-	int		move;
+	int				links_nb;
+	int				i;
+	unsigned short	a;
+	unsigned short	b;
+	unsigned short	c;
 
 	links_nb = 0;
-	move = 0;
-
-	
-
+	i = 0;
+	a = tetriminos >> 12 & tetriminos >> 8;
+	b = tetriminos >> 8 & tetriminos >> 4;
+	c = tetriminos >> 4 & tetriminos;
+	while (i < 4)
+	{
+		links_nb = a >> i & v_mask ? links_nb + 1 : links_nb;
+		links_nb = b >> i & v_mask ? links_nb + 1 : links_nb;
+		links_nb = c >> i & v_mask ? links_nb + 1 : links_nb;
+	 	++i;
+	}
 	return (links_nb);
 }
 
 int		main(void)
 {
-	printf("%d\n", ft_count_vlinks(0b0001000100010001));
+	printf("%d\n", ft_count_vlinks(0b0110001100000000));
+	printf("%d\n", ft_count_hlinks(0b0110001100000000));
 	return (0);
 }
