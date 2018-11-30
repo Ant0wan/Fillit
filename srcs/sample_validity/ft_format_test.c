7a16b032346f12d34857b2a0fbd94e9765a4d458
Ant0wan <abarthel@student.42.fr>
2018-11-30 16:08:25 +0100

    norme format ok

--- srcs/sample_validity/ft_format_test.c
+++ srcs/sample_validity/ft_format_test.c
@@ -6,15 +6,11 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/29 16:08:27 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/30 16:08:08 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "fillit.h"
-#define BUFF_SIZE 1 // temporary
-#include <fcntl.h> // temp
-#include <unistd.h> // temp
-#include <stdio.h> // temp
+#include "fillit.h"
 
 int		ft_format_test(int fd)
 {
@@ -36,8 +32,8 @@ int		ft_format_test(int fd)
 		else if (i != 20 && ((i + 1) % 5 != 0 && !(*buffer == '#'
 						|| *buffer == '.')))
 			return (-1);
-		hash_nb = (i + 1) % 21 ? hash_nb : 0; 
-		i = (i + 1) % 21; 
+		hash_nb = (i + 1) % 21 ? hash_nb : 0;
+		i = (i + 1) % 21;
 	}
 	return (1);
 }
