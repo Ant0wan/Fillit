d6b956672ceb33b5a0eb6be5a427d242db935f81
Antoine BARTHELEMY <abarthel@e2r2p5.42.fr>
2018-11-30 13:45:53 +0100

    all

--- srcs/ft_tetri_validity.c
+++ srcs/ft_tetri_validity.c
@@ -1,15 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_tetri_validity.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/30 11:20:47 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/30 13:45:43 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// is sample and pieces valid ?
+//count nb of pieces
 
 //#include "fillit.h"
 #define BUFF_SIZE 1 // temporary
@@ -17,42 +19,42 @@
 #include <unistd.h> // temp
 #include <stdio.h> // temp
 
-int		ft_format_test(int fd);
-
-int		wrap_test(char *file_path)
+enum	e_masks
 {
-	int	fd;
-	int	check;
+		h_mask = 0b0000000000000011,
+		v_mask = 0b0000000000000001,
+};
 
-	if ((fd = open(file_path, O_RDONLY)) < 0)
-		return (-1);
-	check = ft_format_test(fd);
-	if ((close(fd)) < 0)
-		return (-1);
-	if (check == -1)
-		return (-1);
-	if ((fd = open(file_path, O_RDONLY)) < 0)
-		return (-1);
-//	check = ft_tetri_validity();
-	if ((close(fd)) < 0)
-		return (-1);
-	else
-		return (1);
+static int		ft_count_hlinks(unsigned short tetriminos)
+{
+	int		links_nb;
+	int		move;
+
+	links_nb = 0;
+	move = 0;
+	while (move < 15)
+	{
+		links_nb = (tetriminos >> move & h_mask) == 3 ? links_nb + 1 : links_nb;
+		++move;
+	}
+	return (links_nb);
 }
 
-/*
-** Main for testings
-*/
-
-int		main(int argc, char **argv)
+static int		ft_count_vlinks(unsigned short tetriminos)
 {
-	int		ret;
+	int		links_nb;
+	int		move;
 
-	ret = 0;
-	if (argc == 2)
-	{
-		ret = wrap_test(argv[1]);
-		printf("format_test return: %d\n", ret);
-	}
+	links_nb = 0;
+	move = 0;
+
+	
+
+	return (links_nb);
+}
+
+int		main(void)
+{
+	printf("%d\n", ft_count_vlinks(0b0001000100010001));
 	return (0);
 }
