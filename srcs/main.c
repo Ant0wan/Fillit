16131283bc8847db7cd824b54f178684a37aea0d
Antoine BARTHELEMY <abarthel@e2r2p12.42.fr>
2018-12-03 14:55:59 +0100

    counts nb of tetri in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/30 17:29:31 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/03 14:55:09 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 int		main(int argc, char **argv)
 {
 	(void)argv;
+	int		nb_tetri;
+
+	nb_tetri = 0;
 	if (argc != 2)
 		write(1, "error\n", 6);
 	else
 	{
-		if (issamplevalid(argv[1]) == -1)
+		if ((nb_tetri = issamplevalid(argv[1])) == -1)
 			write(1, "error\n", 6);
 		else
-			write(1, "ok\n", 3); // backtracking algo
+		{
+			printf("nb of tetri: %d\n", nb_tetri);
+			write(1, "Sotckage tetriminos ==> \n", 25); // backtracking algo
+			
+		}
 	}
 	return (0);
 }
