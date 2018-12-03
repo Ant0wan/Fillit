fcdd551540a35bb15b03b96fa9cd8c602a966e22
Ant0wan <abarthel@student.42.fr>
2018-12-03 15:21:45 +0100

    stock tetri ok

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,31 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/03 14:55:09 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/03 15:17:51 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+void	print_tetri_test(unsigned short *tab, int nb) // DEBUGG FT
+{
+	int	i;
+	i = 0;
+	while (i < nb)
+	{
+		printf("elem %d: %d\n", i + 1, tab[i]);
+		++i;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	(void)argv;
 	int		nb_tetri;
+	unsigned short	*tab_tetri;
 
 	nb_tetri = 0;
+	tab_tetri = NULL;
 	if (argc != 2)
 		write(1, "error\n", 6);
 	else
@@ -28,8 +41,11 @@ int		main(int argc, char **argv)
 		{
 			printf("nb of tetri: %d\n", nb_tetri);
 			write(1, "Sotckage tetriminos ==> \n", 25); // backtracking algo
-			
+			tab_tetri = ft_stock_tetri(nb_tetri, argv[1]);
+			print_tetri_test(tab_tetri, nb_tetri);
 		}
+	
+	free(tab_tetri);
 	}
 	return (0);
 }
