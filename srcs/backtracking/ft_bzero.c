3f3ab14f78efb9950f6552b9ed710330c293cb46
Ant0wan <abarthel@student.42.fr>
2018-12-05 16:30:39 +0100

    printing map

--- srcs/backtracking/ft_bzero.c
+++ srcs/backtracking/ft_bzero.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printbyte.c                                     :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/03 16:29:23 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/03 17:01:23 by abarthel         ###   ########.fr       */
+/*   Created: 2018/11/07 14:09:45 by abarthel          #+#    #+#             */
+/*   Updated: 2018/12/05 15:08:00 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "fillit.h"
+#include "fillit.h"
 
-#include <unistd.h>
-
-void	ft_printbyte(unsigned char byte)
+void	ft_bzero(void *s, size_t n)
 {
-	int	i;
-	unsigned char	mask;
+	size_t			i;
+	unsigned char	*s_ptr;
 
-	i = 8;
-	mask = 0b00000001;
-	while (--i >= 0)
-		if ((byte >> i & mask) == 1)
-			write(1, "1", 1);
-		else
-			write(1, "0", 1);
-}
-
-int	main(void)
-{
-	ft_printbyte(0b00010001);
-	return (0);
+	i = -1;
+	s_ptr = (unsigned char*)s;
+	while (++i < n)
+		s_ptr[i] = 0;
 }
