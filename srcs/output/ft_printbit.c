2d77728da59b167f2ba4a78b25763149cd53cb8e
Ant0wan <abarthel@student.42.fr>
2018-12-05 17:20:08 +0100

    print map ok

--- srcs/output/ft_printbit.c
+++ srcs/output/ft_printbit.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 16:29:23 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/05 16:25:00 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/05 17:19:20 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_printbit(unsigned char byte)
 {
-	if ((byte & (1 << 0)) == 1)
+	if ((byte & (1 << 7)) == 128)
 		write(1, "1", 1);
 	else
 		write(1, "0", 1);
