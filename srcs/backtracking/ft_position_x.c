afcea7be8e1c4affe8e18dc0844ffb2c73b9737b
Antoine BARTHELEMY <abarthel@e1r5p8.42.fr>
2018-12-06 17:05:26 +0100

    ft_position_x on the go

--- srcs/backtracking/ft_position_x.c
+++ srcs/backtracking/ft_position_x.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 12:05:10 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/06 15:05:45 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/06 17:02:35 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,16 @@ void	ft_position_x(unsigned char *map, unsigned short tetriminos, int map_nb)
 	unsigned char	row;
 	int				nb_bytes;
 	int	i;
+	int	c;
 
 	row = 0b1111;
 	nb_bytes = map_nb * map_nb;
 	i = 0;
-//	while (i < nb_bytes)
-//	{
-//		map[i / 8] = map[i / 8] | (tetriminos & (row << i ) << i);
+	c = 0;
+	while (++i < nb_bytes)
+	{
 		map[i / 8] = map[i / 8] | (tetriminos >> 8);
 		i += 8;
-//		i += 2;
-		map[i / 8] = map[i / 8] | tetriminos;
-//		i = i + 4;
-//		++i;
-//	}
+		map[i / 8] = map[i / 8] | tetriminos >> 2;
+	}
 }
