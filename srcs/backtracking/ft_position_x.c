29cc3fc3132673205202220dbe59fd0b693cc6fe
Ant0wan <abarthel@student.42.fr>
2018-12-06 17:51:35 +0100

    position

--- srcs/backtracking/ft_position_x.c
+++ srcs/backtracking/ft_position_x.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 12:05:10 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/06 17:02:35 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/06 17:50:46 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ void	ft_position_x(unsigned char *map, unsigned short tetriminos, int map_nb)
 	int				nb_bytes;
 	int	i;
 	int	c;
+	int				diff;
 
 	row = 0b1111;
 	nb_bytes = map_nb * map_nb;
 	i = 0;
 	c = 0;
+	diff = (map_nb - 8) < 1 ? ((map_nb - 8) * -1) : (map_nb - 8);
 	while (++i < nb_bytes)
 	{
-		map[i / 8] = map[i / 8] | (tetriminos >> 8);
+		map[i / 8] = (map[i / 8] << i) | tetriminos >> 8;
 		i += 8;
-		map[i / 8] = map[i / 8] | tetriminos >> 2;
+		map[i / 8] = map[i / 8] | tetriminos;
 	}
 }
