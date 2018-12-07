ab894b780f521e98d55a2f24855a1b22b526a092
Ant0wan <abarthel@student.42.fr>
2018-12-07 14:14:31 +0100

    formula line return

--- srcs/backtracking/ft_position_x.c
+++ srcs/backtracking/ft_position_x.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 12:05:10 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/06 17:50:46 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/07 14:14:02 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,28 @@ void	ft_position_x(unsigned char *map, unsigned short tetriminos, int map_nb)
 	nb_bytes = map_nb * map_nb;
 	i = 0;
 	c = 0;
-	diff = (map_nb - 8) < 1 ? ((map_nb - 8) * -1) : (map_nb - 8);
-	while (++i < nb_bytes)
+	diff = map_nb - 8 < 0 ? (map_nb - 8) * -1 : map_nb - 8;
+	(void)tetriminos;
+	if (i < nb_bytes)
 	{
-		map[i / 8] = (map[i / 8] << i) | tetriminos >> 8;
-		i += 8;
-		map[i / 8] = map[i / 8] | tetriminos;
+		/* First 4 bits in the unsignd short */
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		++i;
+		c += diff + 1;
+//		printf("i  :  %d\n", i);
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		++i;
+		++c;
+//		printf("at i  :  %d\n", i);
+		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
 	}
 }
