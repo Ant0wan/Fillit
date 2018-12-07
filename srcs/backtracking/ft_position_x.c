c4e72545b37d678ff705313ae06ca88ddc16427c
Antoine BARTHELEMY <abarthel@e1r5p8.42.fr>
2018-12-07 15:47:16 +0100

    equation on the go

--- srcs/backtracking/ft_position_x.c
+++ srcs/backtracking/ft_position_x.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 12:05:10 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/07 14:14:02 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/07 15:42:07 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,56 @@ void	ft_position_x(unsigned char *map, unsigned short tetriminos, int map_nb)
 	nb_bytes = map_nb * map_nb;
 	i = 0;
 	c = 0;
-	diff = map_nb - 8 < 0 ? (map_nb - 8) * -1 : map_nb - 8;
+	diff = 8 - map_nb > 0 ? (8 - map_nb) : (map_nb - 8);
+	printf("diff : %d\n", diff);
 	(void)tetriminos;
 	if (i < nb_bytes)
 	{
 		/* First 4 bits in the unsignd short */
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		map[i / 8] = map[i / 8] | 1 << (7 - (diff * 0));
 		++i;
 		++c;
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		map[i / 8] = map[i / 8] | 1 << (6 - (diff * 0));
 		++i;
 		++c;
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		map[i / 8] = map[i / 8] | 1 << (5 - (diff * 0));
 		++i;
 		++c;
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		map[i / 8] = map[i / 8] | 1 << (4 - (diff * 0));
 		++i;
-		c += diff + 1;
-//		printf("i  :  %d\n", i);
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		i += map_nb - 4;
+		map[i / 8] = map[i / 8] | 1 << (7 - (diff * 1));
 		++i;
 		++c;
-//		printf("at i  :  %d\n", i);
-		map[i / 8] = map[i / 8] | 1 << (7 - (c % 8));
+		map[i / 8] = map[i / 8] | 1 << (6 - (diff * 1));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (5 - (diff * 1));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (4 - (diff * 1));
+		++i;
+		i += map_nb - 4;
+		map[i / 8] = map[i / 8] | 1 << (7 - (diff * 2));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (6 - (diff * 2));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (5 - (diff * 2));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << (4 - (diff * 2));
+		++i;
+		i += map_nb - 4;
+		map[i / 8] = map[i / 8] | 1 << (7 - (diff * 3));
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << ((((6 - (diff * 3)) % 8) + 8) % 8);
+		++i;
+		++c;
+		map[i / 8] = map[i / 8] | 1 << ((((5 - (diff * 3)) % 8) + 8) % 8);
+		++i;
+		++c;
 	}
 }
