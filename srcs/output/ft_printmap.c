827a92598e955d13d4351b7ffca5156742684b5f
Ant0wan <abarthel@student.42.fr>
2018-12-10 15:23:13 +0100

    printing map ok

--- srcs/output/ft_printmap.c
+++ srcs/output/ft_printmap.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 16:28:29 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/10 15:20:32 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/10 15:22:46 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@ void	ft_printmap(unsigned int *map, int nb)
 {
 	int l;
 	int c;
-	int i;
-	int max;
 
 	l = -1;
-	i = -1;
-	(void)nb;
-	max = ROW_NB * ROW_NB;
-	while (++l < ROW_NB)
+	while (++l < nb)
 	{
 		c = -1;
-		while (++c < ROW_NB)
+		while (++c < nb)
 		{
-			if (c % 32 == 0)
+			if (c % ROW_NB == 0)
 				write(1, "\n\t", 2);
 			ft_printbit(map[l] >> (ROW_NB - c - 1));
 		}
