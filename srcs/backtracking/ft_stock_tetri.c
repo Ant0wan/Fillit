260286dc4f8711d7e99116a60d9b9fae96cc8583
Ant0wan <abarthel@student.42.fr>
2018-12-10 15:54:58 +0100

    go for move ft

--- srcs/backtracking/ft_stock_tetri.c
+++ srcs/backtracking/ft_stock_tetri.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 13:12:48 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/06 15:33:10 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/10 15:53:20 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,36 @@ static unsigned short	ft_pushleft(unsigned short tetriminos)
 			&& (tetriminos_lefted & l_mask3) != l_mask3
 			&& (tetriminos_lefted & l_mask4) != l_mask4)
 		tetriminos_lefted = tetriminos_lefted << 1;
-	while (tetriminos_lefted == (tetriminos_lefted & ~ (0b1111000000000000)))
+	while (tetriminos_lefted == (tetriminos_lefted & ~(0b1111000000000000)))
 		tetriminos_lefted = tetriminos_lefted << 4;
 	return (tetriminos_lefted);
 }
 
 unsigned short			*ft_stock_tetri(int nb, char *av)
 {
-	unsigned short	*tab_tetri;
+	unsigned short	*tab_tetr;
 	int				i;
 	int				fd;
 	char			buf[BLOCK_SIZE];
 
 	if ((fd = open(av, O_RDONLY)) == -1)
-		return(NULL);
+		return (NULL);
 	else
 	{
-		if (!(tab_tetri = (unsigned short*)ft_memalloc(sizeof(*tab_tetri) * nb)))
+		if (!(tab_tetr = (unsigned short*)ft_memalloc(sizeof(*tab_tetr) * nb)))
 			return (NULL);
 		i = 0;
 		while ((read(fd, buf, BLOCK_SIZE)) > 0)
 		{
-			tab_tetri[i] = ft_pushleft(ft_block_to_tetri(buf));
+			tab_tetr[i] = ft_pushleft(ft_block_to_tetri(buf));
 			++i;
 		}
 	}
 	if ((close(fd)) == -1)
 	{
-		free(tab_tetri);
+		free(tab_tetr);
 		return (NULL);
 	}
 	else
-		return (tab_tetri);
+		return (tab_tetr);
 }
-
