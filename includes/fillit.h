d1e74901b929fe6a9d57106d43aa024f44b35ae9
Antoine BARTHELEMY <abarthel@e1r3p7.42.fr>
2018-12-10 16:51:11 +0100

    test position

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 14:56:46 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/10 16:40:16 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/10 16:49:32 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void			ft_bzero(void *s, size_t n);
 
 void			*ft_memalloc(size_t size);
 
-void			ft_position(unsigned int *map, unsigned short *tab_tetri,
+void			ft_position(unsigned int *map, unsigned short tetriminos,
 		int map_nb);
 
 #endif
--- srcs/backtracking/ft_position.c
+++ srcs/backtracking/ft_position.c
@@ -6,8 +6,19 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 15:56:42 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/10 16:18:03 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/10 16:49:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
+{
+	(void)map_nb;
+//	line 0
+	map[0] = tetriminos;
+//	line 1
 
+//	line 2
+
+//	line 3
+
+}
