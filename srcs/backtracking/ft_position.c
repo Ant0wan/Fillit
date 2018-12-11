0b3e6e9025f6ef7380c520526308a351ab862cff
Ant0wan <abarthel@student.42.fr>
2018-12-11 13:25:55 +0100

    moving right

--- srcs/backtracking/ft_position.c
+++ srcs/backtracking/ft_position.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 21:36:08 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/11 13:21:17 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/11 13:25:33 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 	int n;
 
 	c = ROW_NB / 2;
-	n = ROW_NB - 2 ;
+	n = 0;
 	l = 0;
 	while (c &&
 			(map[l] & ((tetriminos & ~(part0_m)) << c)
@@ -27,6 +27,12 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 			 || map[l + 2] & ((tetriminos & ~(part2_m)) << 8) << c
 			 || map[l + 3] & ((tetriminos & ~(part3_m)) << 12) << c))
 		--c;
+	while (n < ROW_NB / 2 &&
+			((map[l] & ((tetriminos & ~(part0_m)) << c) >> n)
+			 || (map[l + 1] & ((tetriminos & ~(part1_m)) << 4) << c) >> n
+			 || (map[l + 2] & ((tetriminos & ~(part2_m)) << 8) << c) >> n
+			 || (map[l + 3] & ((tetriminos & ~(part3_m)) << 12) << c) >> n))
+		++n;
 	if ((map[l] | ((tetriminos & ~(part0_m)) << c)) == (map[l] ^((tetriminos & ~(part0_m)) << c)) && (map[l + 1] | (((tetriminos & ~(part1_m)) << 4) << c)) == (map[l + 1] ^(((tetriminos & ~(part1_m)) << 4) << c)) && (map[l + 2] | (((tetriminos & ~(part2_m)) << 8) << c)) == (map[l + 2] ^(((tetriminos & ~(part2_m)) << 8) << c)) && (map[l + 3] | (((tetriminos & ~(part3_m)) << 12) << c)) == (map[l + 3] ^(((tetriminos & ~(part3_m)) << 12) << c)))
 	{
 		map[l] = (map[l] | (tetriminos & ~(part0_m)) << c) >> n;
