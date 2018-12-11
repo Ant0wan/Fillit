a45d41d5e99633f0d76a7cc225b79268a7a451d1
Ant0wan <abarthel@student.42.fr>
2018-12-11 14:11:52 +0100

    positionning on the way

--- srcs/backtracking/ft_position.c
+++ srcs/backtracking/ft_position.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 21:36:08 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/11 13:35:07 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/11 14:11:31 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 	int	l;
 	int n;
 
-	c = 0;
-	n = 8;
+	c = ROW_NB / 2;
+	n = 0;
 	l = 0;
 	while (c &&
-			(map[l] & ((tetriminos & ~(part0_m)) << c)
-			 || map[l + 1] & ((tetriminos & ~(part1_m)) << 4) << c
-			 || map[l + 2] & ((tetriminos & ~(part2_m)) << 8) << c
-			 || map[l + 3] & ((tetriminos & ~(part3_m)) << 12) << c))
+			(map[l] & ((tetriminos & ~(part0_m)) << c) >> n
+			 || map[l + 1] & (((tetriminos & ~(part1_m)) << 4) << c) >> n
+			 || map[l + 2] & (((tetriminos & ~(part2_m)) << 8) << c) >> n
+			 || map[l + 3] & (((tetriminos & ~(part3_m)) << 12) << c) >> n))
+	{
 		--c;
+	}
 	if ((map[l] | ((tetriminos & ~(part0_m)) << c)) == (map[l] ^((tetriminos & ~(part0_m)) << c)) && (map[l + 1] | (((tetriminos & ~(part1_m)) << 4) << c)) == (map[l + 1] ^(((tetriminos & ~(part1_m)) << 4) << c)) && (map[l + 2] | (((tetriminos & ~(part2_m)) << 8) << c)) == (map[l + 2] ^(((tetriminos & ~(part2_m)) << 8) << c)) && (map[l + 3] | (((tetriminos & ~(part3_m)) << 12) << c)) == (map[l + 3] ^(((tetriminos & ~(part3_m)) << 12) << c)))
 	{
 		map[l] = map[l] | ((tetriminos & ~(part0_m)) << c) >> n;
@@ -34,5 +36,7 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 		map[l + 2] = map[l + 2] | (((tetriminos & ~(part2_m)) << 8) << c) >> n;
 		map[l + 3] = map[l + 3] | (((tetriminos & ~(part3_m)) << 12) << c) >> n;
 	}
+	printf("c = %d\n", c);
+	printf("\tn = %d\n", n);
 	(void)map_nb;
 }
