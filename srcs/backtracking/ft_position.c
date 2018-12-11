c9ecba234884e6c85253a951832418f3713dd0b5
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-11 13:22:01 +0100

    position

--- srcs/backtracking/ft_position.c
+++ srcs/backtracking/ft_position.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 21:36:08 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/11 13:00:50 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/11 13:21:17 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 	int n;
 
 	c = ROW_NB / 2;
-	n = 0;
+	n = ROW_NB - 2 ;
 	l = 0;
 	while (c &&
 			(map[l] & ((tetriminos & ~(part0_m)) << c)
@@ -29,24 +29,10 @@ void	ft_position(unsigned int *map, unsigned short tetriminos, int map_nb)
 		--c;
 	if ((map[l] | ((tetriminos & ~(part0_m)) << c)) == (map[l] ^((tetriminos & ~(part0_m)) << c)) && (map[l + 1] | (((tetriminos & ~(part1_m)) << 4) << c)) == (map[l + 1] ^(((tetriminos & ~(part1_m)) << 4) << c)) && (map[l + 2] | (((tetriminos & ~(part2_m)) << 8) << c)) == (map[l + 2] ^(((tetriminos & ~(part2_m)) << 8) << c)) && (map[l + 3] | (((tetriminos & ~(part3_m)) << 12) << c)) == (map[l + 3] ^(((tetriminos & ~(part3_m)) << 12) << c)))
 	{
-		map[l] = map[l] | (tetriminos & ~(part0_m)) << c;
-		map[l + 1] = map[l + 1] | ((tetriminos & ~(part1_m)) << 4) << c;
-		map[l + 2] = map[l + 2] | ((tetriminos & ~(part2_m)) << 8) << c;
-		map[l + 3] = map[l + 3] | ((tetriminos & ~(part3_m)) << 12) << c;
+		map[l] = (map[l] | (tetriminos & ~(part0_m)) << c) >> n;
+		map[l + 1] = (map[l + 1] | ((tetriminos & ~(part1_m)) << 4) << c) >> n;
+		map[l + 2] = (map[l + 2] | ((tetriminos & ~(part2_m)) << 8) << c) >> n;
+		map[l + 3] = (map[l + 3] | ((tetriminos & ~(part3_m)) << 12) << c) >> n;
 	}
-//	else
-//	{
-		//Le % 2, permet de dire "tant qu'on a encore des 0" on peut shifter, et a[res jai simplie ton egalite avec un & car au final ca revenait a dire 1 on pose pas donc on shift et 0 on pose)
-//		while (!(((tetriminos & ~(part0_m)) >> n) % 2) &&
-//				(map[l] & ((tetriminos & ~(part0_m)) >> n)))
-//			n++;
-//		if ((map[l] | ((tetriminos & ~(part0_m)) << c)) == (map[l] ^((tetriminos & ~(part0_m)) << c)) && (map[l + 1] | (((tetriminos & ~(part1_m)) << 4) << c)) == (map[l + 1] ^(((tetriminos & ~(part1_m)) << 4) << c)) && (map[l + 2] | (((tetriminos & ~(part2_m)) << 8) << c)) == (map[l + 2] ^(((tetriminos & ~(part2_m)) << 8) << c)) && (map[l + 3] | (((tetriminos & ~(part3_m)) << 12) << c)) == (map[l + 3] ^(((tetriminos & ~(part3_m)) << 12) << c)))
-//		{
-//			map[l] = map[l] | (tetriminos & ~(part0_m)) >> n;
-//			map[l + 1] = map[l + 1] | ((tetriminos & ~(part1_m)) << 4) >> n;
-//			map[l + 2] = map[l + 2] | ((tetriminos & ~(part2_m)) << 8) >> n;
-//			map[l + 3] = map[l + 3] | ((tetriminos & ~(part3_m)) << 12) >> n;
-//		}
-//	}
 	(void)map_nb;
 }
