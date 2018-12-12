85291611ea2637bf288837efd46079c055f22706
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-12 15:59:28 +0100

    easy test added and go backtracking now

--- srcs/backtracking/ft_position_y.c
+++ srcs/backtracking/ft_position_y.c
@@ -6,14 +6,28 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:10:51 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/12 13:36:47 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/12 15:53:32 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	ft_position_y(unsigned int *map, unsigned short tetriminos, int map_nb, int l)
+char	ft_position_y(unsigned int *map, unsigned short tetriminos, int map_nb, int l)
 {
-	if (!(ft_position_x(map, tetriminos, map_nb, l)))
-		ft_position_y(map, tetriminos, map_nb, ++l);
+	char	res;
+	char	height;
+
+	height = ft_height(tetriminos);
+	if ((res = ft_position_x(map, tetriminos, map_nb, l)) && l < map_nb - height)
+	{
+		printf("res: %d\n", res);
+		return (ft_position_y(map, tetriminos, map_nb, ++l));
+	}
+	else if (res && l >= map_nb - height)
+	{
+		printf("2res: %d\n", res);
+		return (1);
+	}
+	else
+		return (0);
 }
