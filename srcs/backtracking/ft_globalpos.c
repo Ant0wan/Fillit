bba031f7c496e0bcdac0f3f0738e9626bcac30e3
Ant0wan <abarthel@student.42.fr>
2018-12-13 14:28:50 +0100

    global positionning trying to change it into rec ft

--- srcs/backtracking/ft_globalpos.c
+++ srcs/backtracking/ft_globalpos.c
@@ -6,13 +6,22 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 14:06:41 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/13 14:13:19 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/13 14:28:12 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	ft_globalpos(unsigned int *map, unsigned short *tab_tetri, char map_nb)
+#include "fillit.h"
+
+char	ft_globalpos(unsigned int *map, unsigned short *tab_tetri,
+		char map_nb, char nb_tetri)
 {
-	if (1)
+	int		i;
+	char	res;
+
+	i = -1;
+	while (++i < nb_tetri)
+		res = ft_position_y(map, tab_tetri[i], map_nb, 0);
+	if (res)
 		return (1);
 	else
 		return (0);
