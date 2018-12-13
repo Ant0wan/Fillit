52270b8ed71e9dcb8a108451df6568b149aad23a
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-13 10:55:50 +0100

    norme on the go

--- srcs/backtracking/ft_position_x.c
+++ srcs/backtracking/ft_position_x.c
@@ -6,13 +6,14 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 21:36:08 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/12 15:23:01 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/13 10:54:18 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	ft_position_x(unsigned int *map, unsigned short tetriminos, int map_nb, int l)
+char	ft_position_x(unsigned int *map, unsigned short tetriminos,
+		int map_nb, int l)
 {
 	int		n;
 	int		c;
@@ -23,9 +24,9 @@ char	ft_position_x(unsigned int *map, unsigned short tetriminos, int map_nb, int
 	thick = ft_thickness(tetriminos);
 	while ((ROW_NB / 2 - c + n + thick < map_nb) &&
 			(map[l] & ((tetriminos & ~(part0_m)) << c) >> n
-			 || map[l + 1] & (((tetriminos & ~(part1_m)) << 4) << c) >> n
-			 || map[l + 2] & (((tetriminos & ~(part2_m)) << 8) << c) >> n
-			 || map[l + 3] & (((tetriminos & ~(part3_m)) << 12) << c) >> n))
+			|| map[l + 1] & (((tetriminos & ~(part1_m)) << 4) << c) >> n
+			|| map[l + 2] & (((tetriminos & ~(part2_m)) << 8) << c) >> n
+			|| map[l + 3] & (((tetriminos & ~(part3_m)) << 12) << c) >> n))
 	{
 		if (c)
 			--c;
@@ -33,13 +34,13 @@ char	ft_position_x(unsigned int *map, unsigned short tetriminos, int map_nb, int
 			++n;
 	}
 	if ((map[l] | (((tetriminos & ~(part0_m)) << c) >> n))
-			== (map[l] ^(((tetriminos & ~(part0_m)) << c) >> n))
+			== (map[l] ^ (((tetriminos & ~(part0_m)) << c) >> n))
 			&& (map[l + 1] | ((((tetriminos & ~(part1_m)) << 4) << c) >> n))
-			== (map[l + 1] ^((((tetriminos & ~(part1_m)) << 4) << c) >> n))
+			== (map[l + 1] ^ ((((tetriminos & ~(part1_m)) << 4) << c) >> n))
 			&& (map[l + 2] | ((((tetriminos & ~(part2_m)) << 8) << c) >> n))
-			== (map[l + 2] ^((((tetriminos & ~(part2_m)) << 8) << c) >> n))
+			== (map[l + 2] ^ ((((tetriminos & ~(part2_m)) << 8) << c) >> n))
 			&& (map[l + 3] | ((((tetriminos & ~(part3_m)) << 12) << c) >> n))
-			== (map[l + 3] ^((((tetriminos & ~(part3_m)) << 12) << c) >> n)))
+			== (map[l + 3] ^ ((((tetriminos & ~(part3_m)) << 12) << c) >> n)))
 	{
 		map[l] = map[l] | ((tetriminos & ~(part0_m)) << c) >> n;
 		map[l + 1] = map[l + 1] | (((tetriminos & ~(part1_m)) << 4) << c) >> n;
--- srcs/backtracking/ft_position_y.c
+++ srcs/backtracking/ft_position_y.c
@@ -6,19 +6,21 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:10:51 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/12 15:53:32 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/13 10:55:26 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	ft_position_y(unsigned int *map, unsigned short tetriminos, int map_nb, int l)
+char	ft_position_y(unsigned int *map, unsigned short tetriminos,
+		int map_nb, int l)
 {
 	char	res;
 	char	height;
 
 	height = ft_height(tetriminos);
-	if ((res = ft_position_x(map, tetriminos, map_nb, l)) && l < map_nb - height)
+	if ((res = ft_position_x(map, tetriminos, map_nb, l))
+			&& l < map_nb - height)
 	{
 		printf("res: %d\n", res);
 		return (ft_position_y(map, tetriminos, map_nb, ++l));
