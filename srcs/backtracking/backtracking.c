d06588f54c9f0c89a3496e26997b8992c0ff95ea
Ant0wan <abarthel@student.42.fr>
2018-12-14 12:27:49 +0100

    positionning new

--- srcs/backtracking/backtracking.c
+++ srcs/backtracking/backtracking.c
@@ -6,11 +6,34 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/13 15:11:29 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/14 12:23:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+char	ft_globalpos(unsigned int *map, unsigned short *tab_tetri,
+		char map_nb, char nb_tetri)
+{
+	static int		i = -1;
+
+	if (++i < nb_tetri && !(ft_position_y(map, tab_tetri[i], map_nb, 0)))
+	{
+		printf("ret 1 i: %d\n", i);
+		return (ft_globalpos(map, tab_tetri, map_nb, nb_tetri));
+	}
+	else if (i < nb_tetri)
+	{
+		++map_nb;
+		--i;
+		printf("ret 2 i: %d\n", i);
+		return (ft_globalpos(map, tab_tetri, map_nb, nb_tetri));
+	}
+	else if (i == nb_tetri)
+		return (0);
+	else
+		return (1);
+}
 
 char	backtracking(char nb_tetri, char *av)
 {
@@ -23,13 +46,7 @@ char	backtracking(char nb_tetri, char *av)
 	map_nb = ft_mapminsize(nb_tetri);
 	tab_tetri = ft_stock_tetri(nb_tetri, av);
 	map = ft_mapgenerator();
-	while (ft_globalpos(map, tab_tetri, map_nb, nb_tetri))
-	{
-		write(1, "T\n", 2);
-		free(map);
-		map = ft_mapgenerator();
-		++map_nb;
-	}
+	ft_globalpos(map, tab_tetri, map_nb, nb_tetri);
 	ft_printmap(map, map_nb);
 	write(1, "\n", 1);
 	free(map);
