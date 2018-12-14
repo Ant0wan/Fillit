d4ffa808036e53620b067b896d827aeeed3c4749
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-14 12:27:49 +0100

    positionning new

--- srcs/backtracking/ft_position_y.c
+++ srcs/backtracking/ft_position_y.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 17:10:51 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/13 14:33:46 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/14 12:26:17 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,23 @@
 char	ft_position_y(unsigned int *map, unsigned short tetriminos,
 		char map_nb, int l)
 {
-	char	res;
 	char	height;
+	char res;
 
 	height = ft_height(tetriminos);
-	if ((res = ft_position_x(map, tetriminos, map_nb, l))
-			&& l < map_nb - height)
+	if (!(res = ft_position_x(map, tetriminos, map_nb, l)))
 	{
-		printf("res: %d\n", res);
-		return (ft_position_y(map, tetriminos, map_nb, ++l));
+		printf("y tetri placed\n");
+		return (0);
 	}
-	else if (res && l >= map_nb - height)
+	else if (res && l < map_nb - height)
 	{
-		printf("placement y failed: %d\n", res);
-		return (1);
+		printf("y placement failed so move down\n");
+		return (ft_position_y(map, tetriminos, map_nb, ++l));
 	}
 	else
 	{
-		printf("placement y succeed: %d\n", res);
-		return (0);
+		printf("y placement totally failed\n");
+		return (1);
 	}
 }
