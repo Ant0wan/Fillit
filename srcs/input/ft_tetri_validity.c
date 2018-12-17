9889b72294121f0567a1f00d07936a31fdbfffca
Ant0wan <abarthel@student.42.fr>
2018-12-17 17:00:48 +0100

    stock on the go

--- srcs/input/ft_tetri_validity.c
+++ srcs/input/ft_tetri_validity.c
@@ -6,55 +6,63 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/13 13:28:28 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/17 17:00:33 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static char		ft_count_hlinks(unsigned short tetriminos)
-{
-	char	links_nb;
-	char	i;
-
-	links_nb = 0;
-	i = 0;
-	while (i < 15)
-	{
-		links_nb = (tetriminos >> i & h_mask) == 3 ? links_nb + 1 : links_nb;
-		++i;
-	}
-	return (links_nb);
-}
-
-static char		ft_count_vlinks(unsigned short tetriminos)
-{
-	char			links_nb;
-	char			i;
-	unsigned short	a;
-	unsigned short	b;
-	unsigned short	c;
-
-	links_nb = 0;
-	i = 0;
-	a = tetriminos >> 12 & tetriminos >> 8;
-	b = tetriminos >> 8 & tetriminos >> 4;
-	c = tetriminos >> 4 & tetriminos;
-	while (i < 4)
-	{
-		links_nb = a >> i & v_mask ? links_nb + 1 : links_nb;
-		links_nb = b >> i & v_mask ? links_nb + 1 : links_nb;
-		links_nb = c >> i & v_mask ? links_nb + 1 : links_nb;
-		++i;
-	}
-	return (links_nb);
-}
-
-char			ft_is_tetri_valid(unsigned short tetriminos)
+static char		ft_count_hlinks(unsigned int *tetri)
 {
 	char	links_nb;
 
-	links_nb = ft_count_vlinks(tetriminos) + ft_count_hlinks(tetriminos);
+	links_nb = 0;
+	links_nb = (tetri[0] & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[1] & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[2] & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[3] & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[0] >> 1 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[1] >> 1 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[2] >> 1 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[3] >> 1 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[0] >> 2 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[1] >> 2 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[2] >> 2 & 3) == 3 ? links_nb + 1 : links_nb;
+	links_nb = (tetri[3] >> 2 & 3) == 3 ? links_nb + 1 : links_nb;
+	return (links_nb);
+}
+
+static char		ft_count_vlinks(unsigned int *tetri)
+{
+	char	links_nb;
+	char	a;
+	char	b;
+	char	c;
+
+	links_nb = 0;
+	a = tetri[0] & tetri[1];
+	b = tetri[1] & tetri[2];
+	c = tetri[2] & tetri[3];
+	links_nb = a & 1 ? links_nb + 1 : links_nb;
+	links_nb = b & 1 ? links_nb + 1 : links_nb;
+	links_nb = c & 1 ? links_nb + 1 : links_nb;
+	links_nb = a >> 1 & 1 ? links_nb + 1 : links_nb;
+	links_nb = b >> 1 & 1 ? links_nb + 1 : links_nb;
+	links_nb = c >> 1 & 1 ? links_nb + 1 : links_nb;
+	links_nb = a >> 2 & 1 ? links_nb + 1 : links_nb;
+	links_nb = b >> 2 & 1 ? links_nb + 1 : links_nb;
+	links_nb = c >> 2 & 1 ? links_nb + 1 : links_nb;
+	links_nb = a >> 3 & 1 ? links_nb + 1 : links_nb;
+	links_nb = b >> 3 & 1 ? links_nb + 1 : links_nb;
+	links_nb = c >> 3 & 1 ? links_nb + 1 : links_nb;
+	return (links_nb);
+}
+
+char			ft_is_tetri_valid(unsigned int *tetri)
+{
+	char	links_nb;
+
+	links_nb = ft_count_vlinks(tetri) + ft_count_hlinks(tetri);
 	if (links_nb == 3 || links_nb == 4)
 		return (1);
 	else
