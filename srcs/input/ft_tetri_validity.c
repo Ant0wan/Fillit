c0fa49d8608d4a87a49315c62d1326f67ff12e55
Ant0wan <abarthel@student.42.fr>
2018-12-17 17:58:58 +0100

    ok

--- srcs/input/ft_tetri_validity.c
+++ srcs/input/ft_tetri_validity.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:21:47 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/17 17:21:56 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/17 17:56:50 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,22 +58,22 @@ static char		ft_count_vlinks(unsigned int *tetri)
 	return (links_nb);
 }
 
-static unsigned short	ft_pushleft(unsigned short tetriminos)
-{
-	unsigned short	tetriminos_lefted;
+//static unsigned short	ft_pushleft(unsigned short tetriminos)
+//{
+//	unsigned short	tetriminos_lefted;
+//
+//	tetriminos_lefted = tetriminos;
+//	while ((tetriminos_lefted & l_mask1) != l_mask1
+//			&& (tetriminos_lefted & l_mask2) != l_mask2
+//			&& (tetriminos_lefted & l_mask3) != l_mask3
+//			&& (tetriminos_lefted & l_mask4) != l_mask4)
+//		tetriminos_lefted = tetriminos_lefted << 1;
+//	while (tetriminos_lefted == (tetriminos_lefted & ~(up_mask)))
+//		tetriminos_lefted = tetriminos_lefted << 4;
+//	return (tetriminos_lefted);
+//}
 
-	tetriminos_lefted = tetriminos;
-	while ((tetriminos_lefted & l_mask1) != l_mask1
-			&& (tetriminos_lefted & l_mask2) != l_mask2
-			&& (tetriminos_lefted & l_mask3) != l_mask3
-			&& (tetriminos_lefted & l_mask4) != l_mask4)
-		tetriminos_lefted = tetriminos_lefted << 1;
-	while (tetriminos_lefted == (tetriminos_lefted & ~(up_mask)))
-		tetriminos_lefted = tetriminos_lefted << 4;
-	return (tetriminos_lefted);
-}
-
-static char			ft_is_tetri_valid(unsigned int *tetri)
+char			ft_is_tetri_valid(unsigned int *tetri)
 {
 	char	links_nb;
 
@@ -81,5 +81,5 @@ static char			ft_is_tetri_valid(unsigned int *tetri)
 	if (links_nb == 3 || links_nb == 4)
 		return (1);
 	else
-		return (-1);
+		return (0);
 }
