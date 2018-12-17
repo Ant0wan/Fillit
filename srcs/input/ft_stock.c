ad86c7db1b7e266de78b4586a8a84a3381f42d26
Antoine BARTHELEMY <abarthel@e2r2p3.42.fr>
2018-12-17 19:36:18 +0100

    input check and table ok

--- srcs/input/ft_stock.c
+++ srcs/input/ft_stock.c
@@ -6,7 +6,7 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 14:44:34 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/17 19:01:45 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/17 19:32:26 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,9 @@ static unsigned int	assign_tetri(char *s)
 	return (shape);
 }
 
-static void	ft_pushright(unsigned int *tetri)
+static void			ft_pushright(unsigned int *tetri)
 {
-	while ((tetri[0] & 1) != 1 
+	while ((tetri[0] & 1) != 1
 			&& (tetri[1] & 1) != 1
 			&& (tetri[2] & 1) != 1
 			&& (tetri[3] & 1) != 1)
@@ -58,49 +58,52 @@ static void	ft_pushright(unsigned int *tetri)
 	}
 }
 
-static char	ft_height(unsigned short tetriminos)
+static char			ft_height(unsigned int *tetri)
 {
-	if (tetriminos & ~(part3_m))
+	if (tetri[3])
 		return (4);
-	else if ((tetriminos & ~(part2_m)) >> 4)
+	else if (tetri[2])
 		return (3);
-	else if ((tetriminos & ~(part1_m)) >> 8)
+	else if (tetri[1])
 		return (2);
 	else
 		return (1);
 }
 
-static char		ft_width(unsigned short tetriminos)
+static char			ft_width(unsigned int *tetri)
 {
 	char	d;
+	char	i;
 
-	d = -1;
-	while (++d < 3)
+	d = 0;
+	i = 4;
+	while (--i)
 	{
-		if (((tetriminos & ~(part0_m)) >> 12) & (v_mask << d))
+		if ((tetri[0] & (1 << i)))
 			return (4 - d);
-		else if (((tetriminos & ~(part1_m)) >> 8) & (v_mask << d))
+		else if ((tetri[1] & (1 << i)))
 			return (4 - d);
-		else if (((tetriminos & ~(part2_m)) >> 4) & (v_mask << d))
+		else if ((tetri[2] & (1 << i)))
 			return (4 - d);
-		else if ((tetriminos & ~(part3_m)) & (v_mask << d))
+		else if ((tetri[3] & (1 << i)))
 			return (4 - d);
+		++d;
 	}
 	return (1);
 }
 
-t_lst	*ft_stock(char *buf, char nb_tetri)
+t_lst				*ft_stock(char *buf, char nb_tetri)
 {
 	char	i;
 	t_lst	*tab;
 
 	if (!(tab = (t_lst*)ft_memalloc(sizeof(t_lst) * nb_tetri)))
 		return (NULL);
-	i = 0;
-	while (i < nb_tetri)
+	i = -1;
+	while (++i < nb_tetri)
 	{
 		tab[i].tetri[0] = assign_tetri(buf);
-		tab[i].tetri[1] = assign_tetri((buf + 5)); 
+		tab[i].tetri[1] = assign_tetri((buf + 5));
 		tab[i].tetri[2] = assign_tetri((buf + 10));
 		tab[i].tetri[3] = assign_tetri((buf + 15));
 		if (ft_is_tetri_valid(tab[i].tetri))
@@ -110,11 +113,9 @@ t_lst	*ft_stock(char *buf, char nb_tetri)
 			free(tab);
 			return (NULL);
 		}
-		tab[i].width = ft_width(tab[i].tetri); // to change
-		tab[i].height = ft_height(tab[i].tetri); // to change
+		tab[i].width = ft_width(tab[i].tetri);
+		tab[i].height = ft_height(tab[i].tetri);
 		tab[i].x = 32 - tab[i].width;
-		tab[i].y = 0;
-		++i;
 		buf += 21;
 	}
 	return (tab);
