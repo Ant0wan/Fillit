d428dc2704a6fbc1f9e7e44d277edacb25fd552e
Ant0wan <abarthel@student.42.fr>
2018-12-18 12:32:54 +0100

    list ok and nb tetri ok using double arguments

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/18 11:38:48 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/18 12:31:03 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@
 int		main(int argc, char **argv)
 {
 	unsigned char	nb_tetri;
+	t_lst			*tab;
+
+	tab = NULL;
 	if (argc != 2)
 		write(1, "error\n", 6);
 	else
 	{
-		printf("nb_tetri (issamplereturn):  %d\n", issamplevalid(argv[1]));
-		if (!(nb_tetri = issamplevalid(argv[1])))
-			write(1, "error\n", 6);
+		printf("nb_tetri (issamplereturn):  %d\n", issamplevalid(&tab, argv[1]));
+//		if (!(nb_tetri = issamplevalid(tab, argv[1])))
+//			write(1, "error\n", 6);
 //		else
-//			backtracking(nb_tetri);
+//			backtracking(tab, nb_tetri);
+		printf("tet: %d\n", (int)tab[0].tetri[0]);
 	}
 	return (0);
 }
