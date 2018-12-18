13eddeaefb18b429c07f6f37c9586f83b613e089
Ant0wan <abarthel@student.42.fr>
2018-12-18 11:40:31 +0100

    memalloc on buffer

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/17 19:42:43 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/18 11:38:48 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 
 int		main(int argc, char **argv)
 {
+	unsigned char	nb_tetri;
 	if (argc != 2)
 		write(1, "error\n", 6);
 	else
 	{
-		printf("issample %d\n", issamplevalid(argv[1]));
+		printf("nb_tetri (issamplereturn):  %d\n", issamplevalid(argv[1]));
 		if (!(nb_tetri = issamplevalid(argv[1])))
 			write(1, "error\n", 6);
-		else
-			backtracking(nb_tetri);
+//		else
+//			backtracking(nb_tetri);
 	}
 	return (0);
 }
