ee361aa06665bef4681c9b3d8fc9d702a5a2eb04
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-19 18:45:00 +0100

    branch

--- srcs/backtracking/backtracking.c
+++ srcs/backtracking/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/18 17:03:22 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/19 18:43:50 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,45 +37,91 @@ static char			ft_mapminsize(unsigned char nb_tetri)
 	return (i);
 }
 
-static char			tetri_feeder(t_lst **tab, unsigned int *map,
-		unsigned char nb_tetri, char map_nb)
-{
-	static int	n = 0;
-
-	if (n < nb_tetri && ft_position_y(tab, map, n))
-	{
+//static char			tetri_feeder(t_lst **tab, unsigned int *map,
+//		unsigned char nb_tetri, char map_nb)
+//{
+//	static int	n = 0;
+//
+//	if (n < nb_tetri && ft_position_y(tab, map, n))
+//	{
 //		write(1, "O", 1);
-		(*tab)[(int)n].x = 32 - (*tab)[(int)n].width;
-		(*tab)[(int)n].y = 0;
-		n = n - 1;
-		printf("n: %d\n", n);
-		if (n == -1)
-			return (1);
-		else
+//		(*tab)[(int)n].x = 32 - (*tab)[(int)n].width;
+//		(*tab)[(int)n].y = 0;
+//		n = n - 1;
+//		printf("n: %d\n", n);
+//		if (n == -1)
+//			return (1);
+//		else
+//		{
+//			ft_placerm(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x);
+//			if ((*tab)[(int)n].x > 0)
+//			{
+//				(*tab)[(int)n].x -= 1;
+//				return (tetri_feeder(tab, map, nb_tetri, map_nb));
+//			}
+//			else if ((*tab)[(int)n].y > 31 - (*tab)[(int)n].height)
+//			{
+//				(*tab)[(int)n].y += 1;
+//				(*tab)[(int)n].x = 32 - (*tab)[(int)n].width;
+//				return (tetri_feeder(tab, map, nb_tetri, map_nb));
+//			}
+//			else
+//				return (1);
+//		}
+//	}
+//	else if (n < nb_tetri)
+//	{
+//		++n;
+//		return (tetri_feeder(tab, map, nb_tetri, map_nb));
+//	}
+//	else
+//		return (0);
+//}
+
+void printBits(size_t const size, void const * const ptr)
+{
+	unsigned char *b = (unsigned char*) ptr;
+	unsigned char byte;
+	int i, j;
+
+	for (i=size-1;i>=0;i--)
+	{
+		for (j=7;j>=0;j--)
 		{
-			ft_placerm(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x);
-			if ((*tab)[(int)n].x > 0)
-			{
-				(*tab)[(int)n].x -= 1;
-				return (tetri_feeder(tab, map, nb_tetri, map_nb));
-			}
-			else if ((*tab)[(int)n].y > 31 - (*tab)[(int)n].height)
-			{
-				(*tab)[(int)n].y += 1;
-				(*tab)[(int)n].x = 32 - (*tab)[(int)n].width;
-				return (tetri_feeder(tab, map, nb_tetri, map_nb));
-			}
-			else
-				return (1);
+			byte = (b[i] >> j) & 1;
+			printf("%u", byte);
 		}
 	}
-	else if (n < nb_tetri)
+	puts("");
+}
+
+static char			tetri_feeder(t_lst **tab, unsigned int *map, unsigned char nb_tetri, char map_nb)
+{
+	int	n;
+
+	n = 0;
+	while (n < nb_tetri)
 	{
-		++n;
-		return (tetri_feeder(tab, map, nb_tetri, map_nb));
+		printf("tetri n %d\n", n);
+		printf("tetri [0]value %d\n", (*tab)[(int)n].tetri[0]);
+		printf("tetri [1]value %d\n", (*tab)[(int)n].tetri[1]);
+		printf("tetri [2]value %d\n", (*tab)[(int)n].tetri[2]);
+		printf("tetri [3]value %d\n", (*tab)[(int)n].tetri[3]);
+		if (!(ft_position_x(tab, map, n, 32)))
+		{
+			printBits(sizeof(map[0]), &map[0]);
+			printBits(sizeof(map[1]), &map[1]);
+			printBits(sizeof(map[2]), &map[2]);
+			printBits(sizeof(map[3]), &map[3]);
+			printBits(sizeof(map[4]), &map[4]);
+			printBits(sizeof(map[5]), &map[5]);
+			printf("\n");
+			++n;
+		}
+		else
+			return (1);
 	}
-	else
-		return (0);
+	return (0);
 }
 
 void				backtracking(t_lst **tab, unsigned char nb_tetri)
@@ -86,6 +132,7 @@ void				backtracking(t_lst **tab, unsigned char nb_tetri)
 
 	map = ft_mapgenerator();
 	map_nb = ft_mapminsize(nb_tetri);
+	map_nb = 32;
 	while ((test = tetri_feeder(tab, map, nb_tetri, map_nb)) && map_nb < ROW_NB + 1)
 		++map_nb;
 	printf("map final size: %d\n", (int)map_nb);
