6b0d8414023a56b31466d1ce4c8f83462b76435c
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-19 19:06:51 +0100

    position x ok

--- srcs/backtracking/backtracking.c
+++ srcs/backtracking/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/19 18:43:50 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/19 19:05:52 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ static char			ft_mapminsize(unsigned char nb_tetri)
 //		return (0);
 //}
 
-void printBits(size_t const size, void const * const ptr)
+void printBits(int n, size_t const size, void const * const ptr)
 {
 	unsigned char *b = (unsigned char*) ptr;
 	unsigned char byte;
@@ -107,19 +107,15 @@ static char			tetri_feeder(t_lst **tab, unsigned int *map, unsigned char nb_tetr
 		printf("tetri [1]value %d\n", (*tab)[(int)n].tetri[1]);
 		printf("tetri [2]value %d\n", (*tab)[(int)n].tetri[2]);
 		printf("tetri [3]value %d\n", (*tab)[(int)n].tetri[3]);
-		if (!(ft_position_x(tab, map, n, 32)))
-		{
-			printBits(sizeof(map[0]), &map[0]);
-			printBits(sizeof(map[1]), &map[1]);
-			printBits(sizeof(map[2]), &map[2]);
-			printBits(sizeof(map[3]), &map[3]);
-			printBits(sizeof(map[4]), &map[4]);
-			printBits(sizeof(map[5]), &map[5]);
-			printf("\n");
-			++n;
-		}
-		else
-			return (1);
+		ft_position_x(tab, map, n, 32 - map_nb);
+		printBits(n, sizeof(map[0]), &map[0]);
+		printBits(n, sizeof(map[1]), &map[1]);
+		printBits(n, sizeof(map[2]), &map[2]);
+		printBits(n, sizeof(map[3]), &map[3]);
+		printBits(n, sizeof(map[4]), &map[4]);
+		printBits(n, sizeof(map[5]), &map[5]);
+		printf("\n");
+		++n;
 	}
 	return (0);
 }
@@ -132,8 +128,8 @@ void				backtracking(t_lst **tab, unsigned char nb_tetri)
 
 	map = ft_mapgenerator();
 	map_nb = ft_mapminsize(nb_tetri);
-	map_nb = 32;
-	while ((test = tetri_feeder(tab, map, nb_tetri, map_nb)) && map_nb < ROW_NB + 1)
+	map_nb = 5;
+	while ((test = tetri_feeder(tab, map, nb_tetri, map_nb)) && map_nb < ROW_NB)
 		++map_nb;
 	printf("map final size: %d\n", (int)map_nb);
 	printf("0 OK, 1 KO: %d\n", (int)test);
--- srcs/backtracking/ft_position.c
+++ srcs/backtracking/ft_position.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 21:36:08 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/19 18:37:38 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/19 19:06:20 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,36 @@
 
 static char	ft_istherespace(unsigned int *map, unsigned int *tetri, char x)
 {
-	if (x > -1)
-	{
-		if (map[0] & (tetri[0] << x))
-			return (1);
-		else if (map[1] & (tetri[1] << x))
-			return (1);
-		else if (map[2] & (tetri[2] << x))
-			return (1);
-		else if (map[3] & (tetri[3] << x))
-			return (1);
-		else
-			return (0);
-	}
+	if (map[0] & (tetri[0] << x))
+		return (1);
+	else if (map[1] & (tetri[1] << x))
+		return (1);
+	else if (map[2] & (tetri[2] << x))
+		return (1);
+	else if (map[3] & (tetri[3] << x))
+		return (1);
 	else
 		return (0);
 }
 
 char	ft_position_x(t_lst **tab, unsigned int *map, unsigned char n, char limit)
 {
-	while (ft_istherespace(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x))
+	char istherespace;
+
+	istherespace = 0;
+	printf("n in x %d\n", (int)n);
+	while ((*tab)[(int)n].x >= limit && (istherespace = ft_istherespace(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x)))
 		(*tab)[(int)n].x -= 1;
-	if ((*tab)[(int)n].x != -1)
+	if (!(istherespace))
 	{
 		ft_placerm(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x);
 		return (0);
 	}
 	else
+	{
+		(*tab)[(int)n].x = 0;
 		return (1);
+	}
 }
 
 //char		ft_position_y(t_lst **tab, unsigned int *map, unsigned char n)
