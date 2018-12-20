560c98ea7a3115fbeb76aa8b1f2cb2a6494e1a96
Ant0wan <abarthel@student.42.fr>
2018-12-20 16:03:26 +0100

    backtracking working

--- srcs/backtracking/backtracking.c
+++ srcs/backtracking/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 17:21:00 by abarthel          #+#    #+#             */
-/*   Updated: 2018/12/20 12:59:09 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/20 15:53:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,50 @@ void printBits(size_t const size, void const * const ptr)
 	puts("");
 }
 
+static char	ft_nselector(t_lst **tab, char map_nb, int n, unsigned int *map)
+{
+//	printf("START BACK");
+	while (--n >= 0)
+	{
+//		printf("n = %d\n", n);
+//		printf("x = %d\n", (int)(*tab)[(int)n].x);
+//		printf("y = %d\n", (int)(*tab)[(int)n].y);
+//		printf("diffx = %d\n", ROW_NB - map_nb);
+//		printf("diffy = %d\n", map_nb - (*tab)[(int)n].height);
+		ft_placerm(&(map[(int)(*tab)[(int)n].y]), (*tab)[(int)n].tetri, (*tab)[(int)n].x);
+//	printBits(sizeof(map[0]), &map[0]);
+//	printBits(sizeof(map[1]), &map[1]);
+//	printBits(sizeof(map[2]), &map[2]);
+//	printBits(sizeof(map[3]), &map[3]);
+//	printBits(sizeof(map[4]), &map[4]);
+//	printBits(sizeof(map[5]), &map[5]);
+//	printBits(sizeof(map[6]), &map[6]);
+//	printBits(sizeof(map[7]), &map[7]);
+//	printBits(sizeof(map[8]), &map[8]);
+//	printBits(sizeof(map[9]), &map[9]);
+//	printBits(sizeof(map[10]), &map[10]);
+//	printBits(sizeof(map[11]), &map[11]);
+//	printBits(sizeof(map[12]), &map[12]);
+//	printBits(sizeof(map[13]), &map[13]);
+//	printBits(sizeof(map[14]), &map[14]);
+//	printBits(sizeof(map[15]), &map[15]);
+		if ((*tab)[(int)n].x > ROW_NB - map_nb)
+		{
+//			printf("1st");
+			(*tab)[(int)n].x -= 1;
+			return (n);
+		}
+		else if ((*tab)[(int)n].y < map_nb - (*tab)[(int)n].height)
+		{
+//			printf("2st");
+			(*tab)[(int)n].y += 1;
+			(*tab)[(int)n].x = ROW_NB - (*tab)[(int)n].width;
+			return (n);
+		}
+	}
+	return (-1);
+}
+
 static char			tetri_feeder(t_lst **tab, unsigned int *map, unsigned char nb_tetri, char map_nb)
 {
 	static int	n = 0;
@@ -62,8 +106,9 @@ static char			tetri_feeder(t_lst **tab, unsigned int *map, unsigned char nb_tetr
 	res = 0;
 	while (n < nb_tetri)
 	{
-		printf("tetri n %d\n", n);
-		printf("map size %d\n", (int)map_nb);
+//		printf("\n");
+//		printf("tetri n %d\n", n);
+//		printf("map size %d\n", (int)map_nb);
 //		printf("tetri x %d\n", (int)(*tab)[(int)n].x);
 //		printf("tetri y %d\n", (int)(*tab)[(int)n].y);
 //		printf("tetri width %d\n", (int)(*tab)[(int)n].width);
@@ -73,70 +118,41 @@ static char			tetri_feeder(t_lst **tab, unsigned int *map, unsigned char nb_tetr
 //		printf("tetri [2]value %d\n", (*tab)[(int)n].tetri[2]);
 //		printf("tetri [3]value %d\n", (*tab)[(int)n].tetri[3]);
 		res = (int)ft_position_y(tab, map, n, map_nb);
-		printf("retour: %d\n", res);
+//		printf("retour: %d\n", res);
 //		printf("tetri new x %d\n", (int)(*tab)[(int)n].x);
 //		printf("tetri new y %d\n\n", (int)(*tab)[(int)n].y);
-	printBits(sizeof(map[0]), &map[0]);
-	printBits(sizeof(map[1]), &map[1]);
-	printBits(sizeof(map[2]), &map[2]);
-	printBits(sizeof(map[3]), &map[3]);
-	printBits(sizeof(map[4]), &map[4]);
-	printBits(sizeof(map[5]), &map[5]);
-	printBits(sizeof(map[6]), &map[6]);
-	printBits(sizeof(map[7]), &map[7]);
-	printBits(sizeof(map[8]), &map[8]);
-	printBits(sizeof(map[9]), &map[9]);
-	printBits(sizeof(map[10]), &map[10]);
-	printBits(sizeof(map[11]), &map[11]);
-	printBits(sizeof(map[12]), &map[12]);
-	printBits(sizeof(map[13]), &map[13]);
-	printBits(sizeof(map[14]), &map[14]);
-	printBits(sizeof(map[15]), &map[15]);
-	printf("\n");
+//	printBits(sizeof(map[0]), &map[0]);
+//	printBits(sizeof(map[1]), &map[1]);
+//	printBits(sizeof(map[2]), &map[2]);
+//	printBits(sizeof(map[3]), &map[3]);
+//	printBits(sizeof(map[4]), &map[4]);
+//	printBits(sizeof(map[5]), &map[5]);
+//	printBits(sizeof(map[6]), &map[6]);
+//	printBits(sizeof(map[7]), &map[7]);
+//	printBits(sizeof(map[8]), &map[8]);
+//	printBits(sizeof(map[9]), &map[9]);
+//	printBits(sizeof(map[10]), &map[10]);
+///	printBits(sizeof(map[11]), &map[11]);
+//	printBits(sizeof(map[12]), &map[12]);
+//	printBits(sizeof(map[13]), &map[13]);
+//	printBits(sizeof(map[14]), &map[14]);
+//	printBits(sizeof(map[15]), &map[15]);
 		if (res)
 		{
-			--n;
-			printf("--n !!!! new n: %d\n", n);
+			n = ft_nselector(tab, map_nb, n, map);
 			if (n == -1)
-			{
-				printf("should before increase\n");
-				n = 0;
 				return (1);
-			}
 			else
-			{
-				ft_placerm(map, (*tab)[(int)n].tetri, (*tab)[(int)n].x);
-					printf("DEP\n x: %d\n", (int)(*tab)[(int)n].x);
-					printf("->\n x lim: %d\n", ROW_NB - map_nb);
-					printf("\n");
-				if ((*tab)[(int)n].x > ROW_NB - map_nb)
-				{
-//					printf("DEP\n x: %d\n", (int)(*tab)[(int)n].x);
-//					printf("->\n x lim: %d\n", ROW_NB - (*tab)[(int)n].width);
-					(*tab)[(int)n].x -= 1;
-					return (tetri_feeder(tab, map, nb_tetri, map_nb));
-				}
-				else if ((*tab)[(int)n].y < map_nb - (*tab)[(int)n].height)
-				{
-					(*tab)[(int)n].y += 1;
-					(*tab)[(int)n].x = ROW_NB - (*tab)[(int)n].width;
-					return (tetri_feeder(tab, map, nb_tetri, map_nb));
-				}
-				else
-				{
-					printf("WTF\n");
-					return (1);
-				}
-			}
+				return (tetri_feeder(tab, map, nb_tetri, map_nb));
 		}
 		else
 		{
 			++n;
-	//		printf("next tetri n %d\n", n);
 		}
 	}
 	return (0);
 }
+
 
 void				backtracking(t_lst **tab, unsigned char nb_tetri)
 {
