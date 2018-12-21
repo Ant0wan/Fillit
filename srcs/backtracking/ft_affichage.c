acf70eb7547bfe290ae0833a9868cb5c393ae119
Anne QUAN <aquan@e2r2p5.42.fr>
2018-12-21 11:00:53 +0100

    affichage almost done

--- srcs/backtracking/ft_affichage.c
+++ srcs/backtracking/ft_affichage.c
@@ -6,22 +6,21 @@
 /*   By: aquan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 17:38:07 by aquan             #+#    #+#             */
-/*   Updated: 2018/12/20 17:58:14 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/12/21 10:59:28 by aquan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
+#include <stdio.h>
 void	*ft_lines(char *str, char map_nb)
 {
 	int i;
 
 	i = 0;
-	while (*str)
+	while (i < (map_nb * (map_nb + 1)))
 	{
 		if ((i + 1 % map_nb) == 0)
-			*str = '\n';
-		++str;
+			str[i] = '\n';
 		++i;
 	}
 	return (0);
@@ -29,37 +28,32 @@ void	*ft_lines(char *str, char map_nb)
 
 void	*ft_place_tetri(char *str, char map_nb, t_lst **tab, char n)
 {	
-	static int 	letter = 0;
-	int			j;
-	int			i;
-	unsigned 	int	k;
+	static int 				letter = 0;
+	unsigned char			j;
+	int						i;
+	unsigned char			k;
+	int index;
 
 	i = 0;
-	while (i++ < 4)
+	while (i < ((*tab)[(int)n].height))
 	{
-		j = ((*tab)[(int)n].y) * (map_nb + 1) + ((*tab)[(int)n].x + j);
-		k = (ROW_NB);
-		while (k > (unsigned int)(ROW_NB - (*tab)[(int)n].width))
-		{
-			if ((k & ((*tab)[(int)n].tetri[0]) ||
-						k & ((*tab)[(int)n].tetri[1]) ||
-						k & ((*tab)[(int)n].tetri[2]) || 
-						k & ((*tab)[(int)n].tetri[3])))
-			{
-				str[j++] = 'A' + letter;
-				k >>= 1;
-			}
-			while (str[j] < 4)
+		j = 0;
+		k = ((*tab)[(int)n].tetri[i]);
+		printf("TEST\n");
+		while (j < ((*tab)[(int)n].width))
+		{		
+			if (((*tab)[(int)n].tetri[i]) & (0x1 << 15))
 			{	
-				(*tab)[(int)n].tetri[0] <<= 1;
-				(*tab)[(int)n].tetri[1] <<= 1;
-				(*tab)[(int)n].tetri[2] <<= 1;
-				(*tab)[(int)n].tetri[3] <<= 1;
+				index = ((*tab)[(int)n].y) * (map_nb + 1) + ((*tab)[(int)n].x + j);
+				str[j++] = 'A' + letter;
+		//		k <<= 1;
 			}
-			while (str[j] && str[j] != '\n')
-				++j;
-			k >>= 1;
+			printf("VALEUR DE J: %d\n", j);
+			printf("VALEUR DE K: %d\n", k);
+			k <<= 1;
+			j++;
 		}
+		i++;
 	}
 	++letter;
 	return (0);
@@ -70,17 +64,19 @@ char	*affichage(char map_nb, t_lst **tab, char nb_tetri)
 	int idx_tetri;
 	char *str;
 
-	str = ft_strnew(map_nb * map_nb + 1);
-	ft_memset(str, '.', map_nb * map_nb + 1);
-	str[map_nb * map_nb + 1] = '\0';
+	if (!(str = (char*)ft_strnew(map_nb * (map_nb + 1))))
+		return (0);
+	ft_memset(str, '.', map_nb * (map_nb + 1));
+//	str[map_nb * map_nb + 1] = '\0';
 	ft_lines(str, map_nb + 1);
 	idx_tetri = 0;
 	while (idx_tetri < nb_tetri)
 	{
+		printf("BOUCLE INFINIE\n"); 
 		ft_place_tetri(str, map_nb, tab, (char)idx_tetri);
 		idx_tetri++;
 	}		
 	ft_putstr(str);
-	ft_memdel((void**)str);
+//	ft_memdel((void**)str);
 	return (0);
 }
