1a7169fcf2f539bfe6caee5e4abf2ac92289e3c1
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2018-12-21 12:16:05 +0100

    output

---
+++ srcs/output/output.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/21 12:05:21 by abarthel          #+#    #+#             */
+/*   Updated: 2018/12/21 12:15:24 by abarthel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+void	ft_output_fillit(t_lst **tab, char nb_tetri, char map_nb)
+{
+	char	*str;
+
+	if (!(len = (char*)ft_memalloc(sizeof(char) * (map_nb + 1) * map_nb + 1)))
+		return;
+}
